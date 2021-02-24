432eb6ffa05987229243dbd21098905e2262369c
Robert O'Connor <robertgoconnor2@gmail.com>
2021-02-23 23:13:00 -0500

    Add home component and new edit input tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,12 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { User } from '../models/user.model';
+import { UserService, AuthenticationService } from '../services';
 import { HomeComponent } from './home.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterTestingModule} from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
+  let comp: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
@@ -24,11 +27,46 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
+    comp = fixture.componentInstance;
+
+    const currentUser: User = {
+      firstName: "John",
+      id: 1,
+      lastName: "Connor",
+      token: "fake-jwt-token",
+      username: "john1",
+      password: "123456"
+    };
+
+    comp.currentUser = currentUser;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(comp).toBeTruthy();
   });
+
+  it('should display the currentUser\'s name', () => {
+    const headerElement: HTMLElement = fixture.nativeElement;
+    const h1 = headerElement.querySelector('h1');
+    
+    expect(h1.textContent).toContain('John');
+  })
+
+  it('should not display the form', () => {
+    const formElement: HTMLElement = fixture.nativeElement;
+    const form = formElement.querySelector('form');
+
+    expect(form).toEqual(null)
+  })
+
+  it('should display the form', () => {
+    comp.editUserDisplay = 'input-show'
+
+    const formElement: HTMLElement = fixture.nativeElement;
+    const form = formElement.querySelector('form');
+
+    expect(form).toBeTruthy();
+  })
 });
